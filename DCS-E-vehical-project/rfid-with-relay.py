05887aa0267991340c618cf53864c8cc07b7387f
Sachin Sharma <67816867+Sachinsharma01@users.noreply.github.com>
2021-02-26 13:22:54 +0530

    Update rfid-with-relay.py
    added thingworx code

--- DCS-E-vehical-project/rfid-with-relay.py
+++ DCS-E-vehical-project/rfid-with-relay.py
@@ -3,10 +3,14 @@ from mfrc522 import SimpleMFRC522
 from time import sleep
 import requests
 import json
+import http.client
 
 led = 40
 relay = 17
 GPIO.setwarnings(False)    # Ignore warning for now
+
+'''Properties'''
+uid
 
 '''Setup all the GPIO pins for the specific tasks'''
 GPIO.setmode(GPIO.BOARD)
@@ -21,13 +25,15 @@ uid = 872589727
 '''Url and API key to upload data on Thingworx'''
 url = "url"
 apiKey = "api"
-header = {'Content-Type' : application/json, appKey : apiKey}
+thingName = "191500402_rfid"
+header = {'ContentType' : 'application/json', 'appKey' : apiKey}
 payLoad = {'Detection' : 'True'}
 while True:
         try:
             id, text = reader.read()
             sleep(2)
             if id == uid:
+                res = requests.put(url+'/Thingworx/Things/'+thingName+'/Properties/*',headers=headers,json,verify=False)
                 GPIO.output(relay, GPIO.HIGH)
                 sleep(2)
                 GPIO.output(led, GPIO.HIGH)
