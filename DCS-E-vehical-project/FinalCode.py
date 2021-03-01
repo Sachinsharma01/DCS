d81fd1f8f7394855e5d80d99dbcfa4b5391fec71
Sachin Sharma <67816867+Sachinsharma01@users.noreply.github.com>
2021-03-01 22:20:21 +0530

    Update FinalCode.py
    updated api and relay pins

--- DCS-E-vehical-project/FinalCode.py
+++ DCS-E-vehical-project/FinalCode.py
@@ -5,48 +5,56 @@ import requests
 import json
 import http.client
 http.client.HTTPConnection.debuglevel=1
-'''led = 40
-relay = 17'''
+led = 8
+relay = 15
 GPIO.setwarnings(False)    # Ignore warning for now
 
 '''Properties'''
-
+property1="UID"
+property2 = "FailureAttempts"
+property3 = "logInAttempts"
+property4 = "userName"
 
 '''Setup all the GPIO pins for the specific tasks'''
-'''GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led,GPIO.OUT)
 GPIO.output(led, GPIO.LOW)
-GPIO.setup(relay, GPIO.OUT)'''
+GPIO.setup(relay, GPIO.OUT)
+GPIO.output(relay, GPIO.LOW)
 
 '''create an object to read the data from rfid'''
 reader = SimpleMFRC522()
-uid = 1031910267405
-property1="UID"
+uid = 727073640024
+
 
 '''Url and API key to upload data on Thingworx'''
 url = "http://dcs.glaitm.org:7080"
 apiKey = "796a8733-b7b6-440a-85b5-577b51f31bad"
 thingName = "191500402_rfid"
 headers = {'Content-Type' : 'application/json', 'appKey' : apiKey}
-'''payLoad = {'Detection' : 'True'}'''
+
+fa = sa =0
+
 while True:
         try:
-            print("try:")
+            print("Introduce Your Card:")
             UID, text = reader.read()
             sleep(2)
             if UID == uid:
                 print(UID)
-                payload={property1:UID}
-                print("SHOW")
-                res = requests.put(url+'/Thingworx/Things/'+thingName+'/Properties/*',headers=headers,json=payload,
-                                   verify=False)
-                '''GPIO.output(relay, GPIO.HIGH)
+                sa += 1
+                print(text)
+                GPIO.output(relay, GPIO.HIGH)
                 sleep(2)
                 GPIO.output(led, GPIO.HIGH)
-                sleep(1)'''
+                sleep(1)
             else:
-                print("Error")
-                '''GPIO.output(relay, GPIO.LOW)
-                GPIO.output(led, GPIO.LOW)'''
+                print(UID)
+                fa +=1
+                GPIO.output(relay, GPIO.LOW)
+                GPIO.output(led, GPIO.LOW)
+            payload={property1:UID, property4: text, property2: fa, property3: sa}
+            res = requests.put(url+'/Thingworx/Things/'+thingName+'/Properties/*',headers=headers,json=payload,
+                                   verify=False)
         except:
                 GPIO.cleanup()
